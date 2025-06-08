db1de239ff9cc393a875ee29413edf62c21c7925
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-06-08 09:28:22 +0100

    including frontend url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,2 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "https://backend-for-fashion-app.onrender.com",
+  "https://fashion-store-app-sigma.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
 module.exports = allowedOrigins;
